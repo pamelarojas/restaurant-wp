d4b5cd82a519bf1e90f74decc7a2b123a0e52832
unknown <p.rojasbaquedano@gmail.com>
2018-10-01 00:25:05 -0300

    cambios

--- wp-content/themes/restaurant/functions.php
+++ wp-content/themes/restaurant/functions.php
@@ -4,19 +4,15 @@ function dl_enqueue_style() {
     $theme_data = wp_get_theme();
 
     wp_register_style('bootstrap', get_theme_file_uri('/assets/css/bootstrap.css'), null, null, 'screen');
-
-    wp_register_style('lightbox', get_theme_file_uri('/assets/css/lightbox.css'), null, $theme_data->get( 'Version' ), 'screen');
     
     wp_register_style('style', get_theme_file_uri('/assets/css/style.css'), null, $theme_data->get( 'Version' ), 'screen');
 
     wp_register_style('fonts-1',"https://use.fontawesome.com/releases/v5.3.1/css/all.css");
     
-    wp_register_style('fonts-2',"https://fonts.googleapis.com/css?family=Archivo+Black|Lato:400,700|Oswald");
+    wp_register_style('fonts-2',"https://fonts.googleapis.com/css?family=Archivo+Black|NoticiaText-Italic|Oswald");
 
     wp_enqueue_style('bootstrap');
- 
-    wp_enqueue_style('lightbox');
- 
+
     wp_enqueue_style('style');
 
       
@@ -37,9 +33,6 @@ function dl_enqueue_scripts() {
 
     wp_register_script('bootstrap', get_theme_file_uri('assets/js/bootstrap.js'), null, null, true);
 
-
-    wp_register_script('lightbox-plus-jquery', get_theme_file_uri('assets/js/lightbox-plus-jquery.min.js'), array('jquery-2'), null, true);
-
     wp_register_script('responsiveslides', get_theme_file_uri('assets/js/responsiveslides.min.js'), array('jquery-2'), null, true);
 
    
@@ -47,7 +40,6 @@ function dl_enqueue_scripts() {
     wp_enqueue_script('jquery-1.11.1');
     wp_enqueue_script('modernizr');
     wp_enqueue_script('bootstrap');
-    wp_enqueue_script('lightbox-plus-jquery');
     wp_enqueue_script('responsiveslides');
    
     
